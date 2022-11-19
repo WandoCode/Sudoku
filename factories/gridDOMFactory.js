c56236ddc0446ac3163e861384f3fbb236c0ad52
Wandole <mchirez@gmail.com>
2022-11-19 23:12:10 +0100

    greateBoard is now a specific method and add a saveGame specific method

--- factories/gridDOMFactory.js
+++ factories/gridDOMFactory.js
@@ -1,21 +1,51 @@
+import gameStore from '../store/game.store.js'
 import rowFactory from './rowDOMFactory.js'
 
-function gridDOMFactory(gridDatas) {
-  const rowGenerator = rowFactory(gridDatas)
+function gridDOMFactory(id, gridDatas) {
+  let board
+  console.log({ id, gridDatas })
+  const toggleValues = (posX, posY) => {
+    const cellData = gridDatas.find((cell) => {
+      return cell.position.x === posX && cell.position.y === posY
+    })
 
-  const createBoard = () => {
-    const table = document.createElement('table')
-    table.classList.add('board')
+    const value = cellData.value
 
-    for (let j = 0; j < 9; j++) {
-      const newRow = rowGenerator.createRow(j)
-      table.appendChild(newRow)
-    }
+    const newValue = value + 1
 
-    return table
+    cellData.value = newValue
+
+    gameStore().saveGame(id, gridDatas)
+    return newValue
   }
 
-  return { createBoard }
+  return {
+    board,
+    createBoard: function () {
+      const rowGenerator = rowFactory(gridDatas)
+
+      const table = document.createElement('table')
+      table.classList.add('board')
+
+      for (let j = 0; j < 9; j++) {
+        const newRow = rowGenerator.createRow(j)
+        table.appendChild(newRow)
+      }
+      this.board = table
+    },
+    addListener: function () {
+      this.board.addEventListener('click', (e) => {
+        const posX = parseInt(e.target.getAttribute('data-pos-x'))
+        const posY = parseInt(e.target.getAttribute('data-pos-y'))
+        const canChange = e.target.getAttribute('data-canChange') === 'true'
+
+        if (canChange) {
+          const newValue = toggleValues(posX, posY)
+          e.target.innerHTML = newValue
+        }
+      })
+    },
+  }
 }
 
 export default gridDOMFactory
