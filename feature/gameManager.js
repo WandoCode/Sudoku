d51be2d430976b421e7b5cdb274f2965f5ba40a7
Wandole <mchirez@gmail.com>
2022-11-19 21:51:56 +0100

    The kept cells have now the propertie 'canChange:false'

--- feature/gameManager.js
+++ feature/gameManager.js
@@ -1,13 +1,26 @@
 import datasFactory from '../factories/datasFactory.js'
+import gameStore from '../store/game.store.js'
+import solutionStore from '../store/solution.store.js'
 
 function gameManager() {
   let gridCurrState = []
   let gridSolution = []
+
+  const saveGame = (gridGame, gridSolution) => {
+    const gameStoreInstance = gameStore()
+    const solutionStoreInstance = solutionStore()
+
+    const id = Date.now()
+
+    solutionStoreInstance.saveSolution(id, gridSolution)
+    gameStoreInstance.saveGame(id, gridGame)
+  }
   return {
     gridCurrState,
     gridSolution,
     launchNewGame: function () {
       const datasMaker = datasFactory()
+
       datasMaker.getRandomFullValidGrid()
 
       this.gridSolution = datasMaker.getGrid()
@@ -15,6 +28,8 @@ function gameManager() {
       datasMaker.makeGridPlayable('easy')
 
       this.gridCurrState = datasMaker.getGrid()
+
+      saveGame(this.gridCurrState, this.gridSolution)
     },
   }
 }
